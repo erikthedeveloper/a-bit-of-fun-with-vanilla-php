c05aa7f2c521f184ab46bef9d61f3490d866ac3b
Erik Aybar <eaybar@practicegenius.com>
2014-09-24 07:19:09 -0600

    Now uses PSR-4 autoloading in favor of rigged up solution

--- includes/classes.php
+++ includes/classes.php
@@ -1,11 +1,34 @@
 <?php
 
-$class_dir  = APP_ROOT . '/MyClasses';
-$iter_dir   = new RecursiveDirectoryIterator($class_dir);
-$iter_iter  = new RecursiveIteratorIterator($iter_dir);
-$iter_regex = new RegexIterator($iter_iter, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
-foreach ($iter_regex as $class_file) {
-    include_once $class_file[0];
-}
+/**
+ * PSR-4 autoloader provided by PSR-4 documentation on GitHub
+ */
+spl_autoload_register(
+    function ($class) {
+        // project-specific namespace prefix
+        $prefix = 'MyClasses\\';
 
-\MyClasses\Models\BaseModel::setPdoConnection($pdo_connection);
+        // base directory for the namespace prefix
+        $base_dir = __DIR__ . '/../MyClasses/';
+
+        // does the class use the namespace prefix?
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
+            // no, move to the next registered autoloader
+            return;
+        }
+
+        // get the relative class name
+        $relative_class = substr($class, $len);
+
+        // replace the namespace prefix with the base directory, replace namespace
+        // separators with directory separators in the relative class name, append
+        // with .php
+        $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+        // if the file exists, require it
+        if (file_exists($file)) {
+            require $file;
+        }
+    }
+);
