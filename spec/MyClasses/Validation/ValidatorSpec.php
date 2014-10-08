c5a7e7d6ce9eae7116b12380c751697219710880
Erik Aybar <eaybar@practicegenius.com>
2014-10-08 06:49:51 -0600

    Refactors tests. Adds validation tests.
    - Rearranges methods
    - it_builds_callable_method_from_validate_rule_name
    - it_validates_an_email_address
    - it_validates_not_empty

--- spec/MyClasses/Validation/ValidatorSpec.php
+++ spec/MyClasses/Validation/ValidatorSpec.php
@@ -32,6 +32,26 @@ class ValidatorSpec extends ObjectBehavior
         )->shouldHaveErrors();
     }
 
+    function it_builds_callable_method_from_validate_rule_name()
+    {
+        $this->translateRuleNameToMethodName('email')->shouldReturn('validateEmail');
+        $this->translateRuleNameToMethodName('not_empty')->shouldReturn('validateNotEmpty');
+        $this->shouldThrow('\InvalidArgumentException')->during('getCallableRuleFromName', ['not_a_valid_rule_name']);
+    }
+
+    function it_validates_an_email_address()
+    {
+        $this->validateEmail('joejoe.com')->shouldReturn(false);
+        $this->validateEmail('joe@joe.com')->shouldReturn(true);
+        $this->validateEmail('joe@joecom')->shouldReturn(false);
+    }
+
+    function it_validates_not_empty()
+    {
+        $this->validateNotEmpty('')->shouldReturn(false);
+        $this->validateNotEmpty('hello there')->shouldReturn(true);
+    }
+
     function it_validates_each_field_with_a_given_array_of_callables()
     {
         $this->validate(
@@ -44,14 +64,6 @@ class ValidatorSpec extends ObjectBehavior
                 'password' => 'foobar'
             ]
         )->shouldHaveErrors();
-    }
-
-    function it_validates_an_email_address()
-    {
-        $this->validate(
-            ['email' => ['email']],
-            ['email' => 'joe@joe.com']
-        )->shouldHaveValidData();
     }
 
     function it_allows_multiple_validation_rules_per_field()
@@ -70,11 +82,5 @@ class ValidatorSpec extends ObjectBehavior
             ['email' => ['not_empty', 'email']],
             ['email' => 'joe@joe.com']
         )->shouldHaveValidData();
-    }
-
-    function it_builds_callable_method_from_validate_rule_name()
-    {
-        $this->getCallableRuleFromName('email')->shouldReturn('validateEmail');
-        $this->getCallableRuleFromName('not_empty')->shouldReturn('validateNotEmpty');
     }
 }
