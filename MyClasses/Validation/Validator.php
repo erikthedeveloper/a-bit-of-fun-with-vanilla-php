b6d284aabe02ef0f0a9913ab0f30b87316fd9ebb
Erik Aybar <eaybar@practicegenius.com>
2014-10-08 13:39:45 -0600

    Unfactor

--- MyClasses/Validation/Validator.php
+++ MyClasses/Validation/Validator.php
@@ -21,17 +21,38 @@ class Validator
      */
     protected $callable_prefix = 'validate';
 
+
     /**
-     * @param $rules
-     * @param $data
-     * @return bool
+     * @param array $rules
+     * @param array $data
+     * @return $this
      * @author Erik Aybar
      */
     public function validate(array $rules, array $data)
     {
-        foreach ($rules as $field_name => $callable_rules) {
-            foreach ($callable_rules as $callable_rule) {
-                $this->validateFieldUsingRule($callable_rule, $field_name, $data[$field_name]);
+        foreach ($rules as $i => $j) {
+            foreach ($j as $k) {
+
+                if (is_callable($k)) {
+                    $passes    = $k($data[$i]);
+                    $k = 'custom validation';
+                } else {
+                    $method_name = $this->callable_prefix . join(
+                            '',
+                            array_map(
+                                'ucfirst',
+                                explode('_', $k)
+                            )
+                        );
+                    if (!method_exists($this, $method_name)) {
+                        throw new InvalidArgumentException("{$k} validation message does not exist");
+                    }
+                    $passes = $this->$method_name($data[$i]);
+                }
+
+                if (!$passes) {
+                    $this->failed_fields[$i] = "Failed the " . str_replace('_', ' ', $k) . " validation";
+                }
             }
         }
         return $this;
@@ -41,18 +62,18 @@ class Validator
      * @return bool
      * @author Erik Aybar
      */
-    public function hasErrors()
+    public function hasValidData()
     {
-        return count($this->failed_fields) > 0;
+        return (!$this->hasErrors());
     }
 
     /**
      * @return bool
      * @author Erik Aybar
      */
-    public function hasValidData()
+    public function hasErrors()
     {
-        return (!$this->hasErrors());
+        return count($this->failed_fields) > 0;
     }
 
     /**
@@ -116,60 +137,6 @@ class Validator
      */
     public function validateEmail($data)
     {
-        return (bool) preg_match('/\w+@\w+\.\w+/', $data);
-    }
-
-    /**
-     * @param $callable_rule_name
-     * @param $field_name
-     * @param $data
-     * @author Erik Aybar
-     */
-    public function validateFieldUsingRule($callable_rule_name, $field_name, $data)
-    {
-        if (is_callable($callable_rule_name)) {
-            $passes = $callable_rule_name($data);
-            $callable_rule_name = 'custom validation';
-        } else {
-            $callable_method = $this->getCallableMethodFromRule($callable_rule_name);
-            $passes          = $this->$callable_method($data);
-        }
-        if (!$passes) {
-            $this->setFieldValidationFailed($field_name, $callable_rule_name);
-        }
-    }
-
-    /**
-     * @param $callable_rule_name
-     * @return string
-     * @author Erik Aybar
-     */
-    public function getCallableMethodFromRule($callable_rule_name)
-    {
-        $callable_method = $this->translateRuleNameToMethodName($callable_rule_name);
-        if (!method_exists($this, $callable_method)) {
-            throw new InvalidArgumentException("{$callable_rule_name} validation message does not exist");
-        }
-        return $callable_method;
-    }
-
-    public function translateRuleNameToMethodName($rule_name)
-    {
-        $method_name = $this->callable_prefix . join('',
-            array_map('ucfirst',
-                explode('_', $rule_name)
-            )
-        );
-        return $method_name;
-    }
-
-    /**
-     * @param $field_name
-     * @param $callable_method
-     * @author Erik Aybar
-     */
-    public function setFieldValidationFailed($field_name, $callable_method)
-    {
-        $this->failed_fields[$field_name] = "Failed the " . str_replace('_', ' ', $callable_method) . " validation";
+        return (bool)preg_match('/\w+@\w+\.\w+/', $data);
     }
 }
